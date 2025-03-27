4d4901fd5067cc6ed1ddda4aa09db76ca950895c
César Guerra <warderer@gmail.com>
2025-03-26 21:06:58 -0600

    ✨ Plantilla Base

--- 02-react-props/src/main.jsx
+++ 02-react-props/src/main.jsx
@@ -5,8 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App saludo='Hola' nombre='David' />
-    <App saludo='Hi' nombre='Katherine'/>
     <App />
   </StrictMode>,
 )
