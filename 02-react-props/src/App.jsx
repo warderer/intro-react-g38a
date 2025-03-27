064cef9796a43e0f85aedc054888e6b8e22ee8e4
César Guerra <warderer@gmail.com>
2025-03-26 21:29:59 -0600

    ✨ Separación de componente HeaderHero

--- 02-react-props/src/App.jsx
+++ 02-react-props/src/App.jsx
@@ -1,3 +1,4 @@
+import HeaderHero from './components/HeaderHero'
 import './App.css'
 import ilustracion from './assets/img/ilustracion.svg'
 import ilustracion1 from './assets/img/ilustracion1.svg'
@@ -12,25 +13,7 @@ function App() {
 
   return (
     <>
-    <header className="hero">
-      <div className="textos-hero">
-        <h1>Bienvenido a Website</h1>
-        <p>Creamos el mejor sitio web para ti</p>
-        <a href="#contacto">Contactame</a>
-      </div>
-      <div className="svg-hero" style={{ height: 150, overflow: "hidden" }}>
-        <svg
-          viewBox="0 0 500 150"
-          preserveAspectRatio="none"
-          style={{ height: "100%", width: "100%" }}
-        >
-          <path
-            d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
-            style={{ stroke: "none", fill: "#fff" }}
-          />
-        </svg>
-      </div>
-    </header>
+    <HeaderHero />
     <section className="wave-contenedor website">
       <img src={ilustracion1} alt="" />
       <div className="contenedor-textos-main">
---
+++ 02-react-props/src/components/HeaderHero.jsx
@@ -0,0 +1,24 @@
+const HeaderHero = () => {
+  return (
+    <header className="hero">
+      <div className="textos-hero">
+        <h1>Bienvenido a Website</h1>
+        <p>Creamos el mejor sitio web para ti</p>
+        <a href="#contacto">Contactame</a>
+      </div>
+      <div className="svg-hero" style={{ height: 150, overflow: "hidden" }}>
+        <svg
+          viewBox="0 0 500 150"
+          preserveAspectRatio="none"
+          style={{ height: "100%", width: "100%" }}
+        >
+          <path
+            d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+            style={{ stroke: "none", fill: "#fff" }}
+          />
+        </svg>
+      </div>
+    </header>
+  )
+}
+export default HeaderHero;
