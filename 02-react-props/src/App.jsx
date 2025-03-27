73084d07c13d9930776d69fab796bb383f487eba
César Guerra <warderer@gmail.com>
2025-03-26 20:51:16 -0600

    🐛 fix pasando props

--- 02-react-props/src/App.jsx
+++ 02-react-props/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App({ props }) {
+function App(props) {
   const [count, setCount] = useState(0)
   console.log(props)
 
