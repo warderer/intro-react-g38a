b2dddc459afeb3bbbe62b4868dc18327e32b836d
César Guerra <warderer@gmail.com>
2025-03-28 20:30:11 -0600

    ✨ Input manejado por estado y boton agregar basico

--- 03-todo-list/src/App.jsx
+++ 03-todo-list/src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 import './App.css'
 
 function App () {
+  /* 1. Los estados en React sirven para guardar información que va a utilizar el componente. Esta información tiene la particularidad de que se actualiza la vista cada vez que cambia (se ejecuta nuevamente el return) */
+  const [inputValue, setInputValue] = useState('')
+
+  // 3b. Función que se ejecuta cuando se hace clic en el botón agregar
+  const handleAdd = () => {
+    console.log('Agregue:', inputValue)
+  }
+
   return (
     <>
       <h1>Lista de Tareas</h1>
-      <input type='text' placeholder='Escribe una tarea' />
-      <button>Agregar</button>
+      {/* 2. Una forma común de trabajar los inputs en React, es usar un evento onChange para guardar la información en el estado apenas esta sea tecleada. La información del input la obtenemos de event.target.value */}
+      <input
+        type='text'
+        placeholder='Escribe una tarea'
+        value={inputValue}
+        onChange={(event) => { setInputValue(event.target.value) }}
+      />
+
+      {/* 3a. Otra forma de trabajar los eventos, es que podemos declarar la función más arriba y solo mandar a llamar el evento */}
+      <button onClick={handleAdd}>Agregar</button>
+
       {/* ul>li{Item $}*3>button{Eliminar} */}
       <ul>
         <li>Item 1<button>Eliminar</button></li>
