c2ff09e5796554f2ae84b49ea2d23edbe278f969
César Guerra <warderer@gmail.com>
2025-03-31 21:30:23 -0600

    ✨ Add SearchBar Component

---
+++ 04-giphy-app/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const SearchBar = ({ handleSearch }) => {
+  const [search, setSearch] = useState('')
+
+  return (
+    <div className='search-bar'>
+      <input
+        className='search-input'
+        type='text'
+        placeholder='Search Gif...'
+        name='search'
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+
+      <button
+        className='search-button'
+        type='button'
+        onClick={() => { handleSearch(search) }}
+      />
+    </div>
+  )
+}
+export default SearchBar
