49986f4af26e7ebfba76131c6bcbf70cadf49896
César Guerra <warderer@gmail.com>
2025-04-02 19:49:50 -0600

    ✨ Add Pokemon list with image (card)

--- 05-poke-bootstrap/src/pages/Home.jsx
+++ 05-poke-bootstrap/src/pages/Home.jsx
@@ -1,6 +1,39 @@
+import { useState, useEffect } from 'react'
+
 const Home = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+      const data = await response.json()
+      setPokemons(data.results)
+    }
+    fetchPokemons()
+  }, [])
+
   return (
-    <h1>Home</h1>
+    <div className='container'>
+      <h1>Home</h1>
+      <div className='row'>
+        {
+        pokemons.map(pokemon => (
+          <div className='col-4' key={pokemon.name}>
+            <div className='card'>
+              <div className='card-body'>
+                <img
+                  className='card-img-top'
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
+                  alt={pokemon.name}
+                />
+                <h5 className='card-title'>{pokemon.name}</h5>
+              </div>
+            </div>
+          </div>
+        ))
+      }
+      </div>
+    </div>
   )
 }
 export default Home
